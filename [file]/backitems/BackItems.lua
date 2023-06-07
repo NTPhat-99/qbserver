e014f336fdf3ed6ac85ab2c3769921a1129c6cd3
NTPhat-99 <nguyenthuanphat.it@gmail.com>
2023-06-07 23:13:30 +0700

    update file new

--- [file]/backitems/BackItems.lua
+++ [file]/backitems/BackItems.lua
@@ -1,17 +1,17 @@
 
 BackItems = {
-    ["markedbills"] = { -- Item Name
-        model="prop_money_bag_01", -- Model you want to display
-        back_bone = 24818, -- PED bone the entity is attached to.
-        -- Location of the model on the players back. Note this is and offset relative to the players bone coords.
-        x = -0.4,
-        y = -0.17,
-        z = -0.12,
-        -- Rotaion of the object.
-        x_rotation = 0.0,
-        y_rotation = 90.0,
-        z_rotation = 0.0,
-    },
+    -- ["markedbills"] = { -- Item Name
+    --     model="prop_money_bag_01", -- Model you want to display
+    --     back_bone = 24818, -- PED bone the entity is attached to.
+    --     -- Location of the model on the players back. Note this is and offset relative to the players bone coords.
+    --     x = -0.4,
+    --     y = -0.17,
+    --     z = -0.12,
+    --     -- Rotaion of the object.
+    --     x_rotation = 0.0,
+    --     y_rotation = 90.0,
+    --     z_rotation = 0.0,
+    -- },
 
     -- ["meth"] = {
     --     model="hei_prop_pill_bag_01", 
@@ -64,24 +64,24 @@ BackItems = {
         y_rotation = 180.0,
         z_rotation = 180.0,
     },
-    ["coke_brick"] = {
-        model="bkr_prop_coke_cutblock_01", 
-        back_bone = 24818,
-        x = -0.20,
-        y = -0.17,
-        z = 0.0,
-        x_rotation = 0.0,
-        y_rotation = 90.0,
-        z_rotation = 90.0,
-    },
-    ["weed_bud"] = {
-        model="bkr_prop_weed_drying_01a", 
-        back_bone = 24818,
-        x = -0.20,
-        y = -0.17,
-        z = 0.0,
-        x_rotation = 0.0,
-        y_rotation = 90.0,
-        z_rotation = 0.0,
-    },
+    -- ["coke_brick"] = {
+    --     model="bkr_prop_coke_cutblock_01", 
+    --     back_bone = 24818,
+    --     x = -0.20,
+    --     y = -0.17,
+    --     z = 0.0,
+    --     x_rotation = 0.0,
+    --     y_rotation = 90.0,
+    --     z_rotation = 90.0,
+    -- },
+    -- ["weed_bud"] = {
+    --     model="bkr_prop_weed_drying_01a", 
+    --     back_bone = 24818,
+    --     x = -0.20,
+    --     y = -0.17,
+    --     z = 0.0,
+    --     x_rotation = 0.0,
+    --     y_rotation = 90.0,
+    --     z_rotation = 0.0,
+    -- },
 }
